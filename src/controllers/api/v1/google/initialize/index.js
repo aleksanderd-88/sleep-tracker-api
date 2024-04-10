ae84b80468c59b35a6aaeb3f3a04ba58c188e5ff
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-04-10 10:28:44 +0200

    Handle api initialization error case

--- src/controllers/api/v1/google/initialize/index.js
+++ src/controllers/api/v1/google/initialize/index.js
@@ -2,10 +2,13 @@ require('dotenv/config')
 
 module.exports = (req, res) => {
   try {
-    res.status(200).send({
-      clientId: process.env.GOOGLE_CLIENT_ID
-    })
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    if ( !clientId )
+      throw new Error('Client id is missing')
+
+    res.status(200).send({ clientId })
   } catch (error) {
-    res.status(500).send('Something went wrong')
+    const errorMessage = error.message || 'Something went wrong'
+    res.status(500).send(errorMessage)
   }
 }
