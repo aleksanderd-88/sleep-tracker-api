1f8902a2dc7638336d0f00a8af00228f2168a3ca
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-04-10 12:52:38 +0200

    Change endpoint path

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 const healthCheck = require('./api/v1/health-check')
-const googleInit = require('./api/v1/google/initialize')
+const googleInit = require('./api/v1/google/one-tap/initialize')
 
 module.exports = {
   healthCheck,
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,4 @@ const controllers = require('../controllers')
 
 module.exports = router.get('/api/v1/health-check', controllers.healthCheck)
 
-module.exports = router.get('/api/v1/google/initialize', controllers.googleInit)
+module.exports = router.get('/api/v1/google/one-tap/initialize', controllers.googleInit)
